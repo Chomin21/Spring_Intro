84188f3be1e7898b1a9caa950b81fe28f6bc2c62
Chomin21 <cmkbooty21@naver.com>
2022-09-08 21:11:27 +0900

    오타 수정

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -6,11 +6,11 @@ import org.springframework.stereotype.Controller;
 
 //컨트롤러쪽은 어쩔 수 없음. 애너테이션 사용 필수.
 @Controller
-public class MemberConfroller {
+public class MemberController {
     private final MemberService memberService;
 
     @Autowired //스프링이 스프링 컨테이너에 있는 memberService를 가져다가 연결을 시켜줌. (DI)
-    public MemberConfroller(MemberService memberService) {
+    public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
 }
